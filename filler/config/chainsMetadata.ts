8a1c28df0ee60d51e89b2f63af0c562ffe6bda35
Haardik <haardik@haardik.dev>
2025-03-27 14:34:45 +0530

    fix rpc url for tall

--- filler/config/chainsMetadata.ts
+++ filler/config/chainsMetadata.ts
@@ -45,7 +45,7 @@ export const chainsMetadata = [
     slug: "tall",
     rpcUrls: {
       default: {
-        http: ["http://127.0.0.1:7547"],
+        http: ["http://127.0.0.1:7550"],
       },
     },
     blockExplorers: undefined,
